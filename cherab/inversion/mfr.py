7614dae1998b0c1c9a7a7e2c51efe75688b9f032
munechika-koyo <munechika.koyo@torus.nr.ttitech.ac.jp>
2023-12-22 18:48:09 +0900

    🐛 Fix the version compatible error for `zip` strict keywards

--- cherab/inversion/mfr.py
+++ cherab/inversion/mfr.py
@@ -358,7 +358,7 @@ class Mfr:
 
         regularization = csc_matrix(self._dmats[0][0].shape, dtype=float)
 
-        for (dmat1, dmat2), aniso in zip(self._dmats, derivative_weights, strict=True):
+        for (dmat1, dmat2), aniso in zip(self._dmats, derivative_weights):  # noqa: B905  for py39
             regularization += aniso * dmat1.T @ w @ dmat2
 
         return regularization
