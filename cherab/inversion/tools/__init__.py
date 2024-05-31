aa008c85bdd7a2f32e60804d160986f1fa38cf9c
munechika-koyo <munechika.koyo@torus.nr.ttitech.ac.jp>
2024-05-31 23:01:26 +0900

    ✨ new tool function to parse exponent representation into Math latex

--- cherab/inversion/tools/__init__.py
+++ cherab/inversion/tools/__init__.py
@@ -1,6 +1,8 @@
 """Subpackage for complementary tools for the inversion."""
+from .scientific_format import parse_scientific_notation
 from .spinner import Spinner
 
 __all__ = [
     "Spinner",
+    "parse_scientific_notation",
 ]
